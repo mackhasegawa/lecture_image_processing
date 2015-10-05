e463566021915737f20ee6431ce426a277c11605
mackhasegawa <hasegawa@mail.dendai.ac.jp>
2015-10-05 15:20:34 +0900

    課題２変更等

--- ex2.m
+++ ex2.m
@@ -1,14 +1,23 @@
+% ‰Û‘è‚Q@ŠK’²”‚Æ‹^——ÖŠs
+% ‚QŠK’²C‚SŠK’²C‚WŠK’²‚Ì‰æ‘œ‚ğ¶¬‚¹‚æD
+
+clear;
+
 ORG=imread('Lenna.png'); % Œ´‰æ‘œ‚Ì“ü—Í
 ORG = rgb2gray(ORG); colormap(gray); colorbar;
-imagesc(ORG); % ‰æ‘œ‚Ì•\¦
+imagesc(ORG); axis image; % ‰æ‘œ‚Ì•\¦
 pause; % ˆê’â~
 
+% ‚QŠK’²‰æ‘œ‚Ì¶¬
 IMG = ORG>128;
-imagesc(IMG); colormap(gray); colorbar;
+imagesc(IMG); colormap(gray); colorbar;  axis image;
 pause;
 
+% ‚SŠK’²‰æ‘œ‚Ì¶¬
 IMG0 = ORG>64;
 IMG1 = ORG>128;
 IMG2 = ORG>192;
-IMG = IMG0 +IMG1 + IMG2;
-imagesc(IMG); colormap(gray); colorbar;
+IMG = IMG0 + IMG1 + IMG2;
+imagesc(IMG); colormap(gray); colorbar;  axis image;
+
+% ‚WŠK’²‚É‚Â‚¢‚Ä‚ÍCŠe©ŒŸ“¢‚µ‚Ä‚­‚¾‚³‚¢D
