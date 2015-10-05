39c9f6d66d4075580b7b56cc5e973b4b39849c86
mackhasegawa <hasegawa@mail.dendai.ac.jp>
2015-10-05 14:11:18 +0900

    課題１修正

--- ex1.m
+++ ex1.m
@@ -1,18 +1,31 @@
+% 課題１　標本化感覚と空間解像度
+% 画像をダウンサンプリングして（標本化間隔を大きくして）
+% 表示せよ．「Lenna」以外の画像を用いよ．
+
 ORG=imread('Lenna.png'); % 原画像の入力
-imagesc(ORG); % 画像の表示
+imagesc(ORG); axis image; % 画像の表示
 pause; % 一時停止
 
-IMG = imresize(ORG,0.5);
-imagesc(IMG);
-pause;
+IMG = imresize(ORG,0.5); % 画像の縮小
+IMG2 = imresize(IMG,2,'box'); % 画像の拡大
+imagesc(IMG2); axis image; % 画像の表示
+pause; % 一時停止
 
-IMG = imresize(IMG,0.5);
-imagesc(IMG);
-pause;
+IMG = imresize(IMG,0.5); % 画像の縮小
+IMG2 = imresize(IMG,4,'box'); % 画像の拡大
+imagesc(IMG2); axis image; % 画像の表示
+pause; % 一時停止
 
-IMG = imresize(IMG,0.5);
-imagesc(IMG);
-pause;
+IMG = imresize(IMG,0.5); % 画像の縮小
+IMG2 = imresize(IMG,8,'box'); % 画像の拡大
+imagesc(IMG2); axis image; % 画像の表示
+pause; % 一時停止
 
-IMG = imresize(IMG,0.5);
-imagesc(IMG);
+IMG = imresize(IMG,0.5); % 画像の縮小
+IMG2 = imresize(IMG,16,'box'); % 画像の拡大
+imagesc(IMG2); axis image; % 画像の表示
+pause; % 一時停止
+
+IMG = imresize(IMG,0.5); % 画像の縮小
+IMG2 = imresize(IMG,32,'box'); % 画像の拡大
+imagesc(IMG2); axis image; % 画像の表示
