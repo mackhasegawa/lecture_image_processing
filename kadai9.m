c63ef989b801818ebf17850a4cd70aeb2d803a8d
mackhasegawa <hasegawa@mail.dendai.ac.jp>
2015-11-30 10:41:06 +0900

    課題９の追加

---
+++ kadai9.m
@@ -0,0 +1,22 @@
+% 課題９ メディアンフィルタと先鋭化
+% メディアンフィルターを適用し，ノイズ除去を体験せよ．
+% 各自，Lenna以外の画像を用いよ．
+% 例
+
+ORG = imread('Lenna.jpg'); % 画像の読み込み
+ORG = rgb2gray(ORG); % 白黒濃淡画像に変換
+imagesc(ORG); colormap(gray); colorbar; % 画像の表示
+pause;
+ORG = imnoise(ORG,'salt & pepper',0.02); % ノイズ添付
+imagesc(ORG); colormap(gray); colorbar; % 画像の表示
+pause;
+IMG = filter2(fspecial('average',3),ORG); % 平滑化フィルタで雑音除去
+imagesc(IMG); colormap(gray); colorbar; % 画像の表示
+pause;
+IMG = medfilt2(ORG,[3 3]); % メディアンフィルタで雑音除去
+imagesc(IMG); colormap(gray); colorbar; % 画像の表示
+pause;
+f=[0,-1,0;-1,5,-1;0,-1,0]; % フィルタの設計
+IMG = filter2(f,IMG,'same'); % フィルタの適用
+imagesc(IMG); colormap(gray); colorbar; % 画像の表示
+pause;
