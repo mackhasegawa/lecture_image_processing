4016ff84bef7058b7f7601113183eee06f7acaf2
Kouno <d86ysv3k@gmail.com>
2016-07-13 21:30:25 +0900

    Add files via upload

--- kadai3.m
+++ kadai3.m
@@ -1,28 +1,34 @@
-% 課題３　閾値処理
-% 閾値を4パターン設定し,閾値処理た画像を示せ．
-% 下記はサンプルプログラムである．
-% 課題作成にあたっては「Lenna」以外の画像を用いよ．
+% 課題３　閾値処理 
+ % 閾値を4パターン設定し,閾値処理た画像を示せ． 
+ % 下記はサンプルプログラムである． 
+ % 課題作成にあたっては「Lenna」以外の画像を用いよ． 
+ 
+ 
+ clear; % 変数のオールクリア 
+ 
+ 
+ ORG=imread('http://ks.c.yimg.jp/res/chie-que-10141/10/141/933/305/i320'); % 原画像の入力 
+ ORG= rgb2gray(ORG); % カラー画像を白黒濃淡画像へ変換 
+ 
+ 
+ imagesc(ORG); colormap(gray); colorbar; % 画像の表示 
+ pause; 
+ 
+ 
+ IMG = ORG > 64; % 輝度値が64以上の画素を1，その他を0に変換 
+ imagesc(IMG); colormap(gray); colorbar; 
+ pause; 
+ 
+ 
+ IMG = ORG > 96; 
+ imagesc(IMG); colormap(gray); colorbar; 
+ pause; 
+ 
+ 
+ IMG = ORG > 128; 
+ imagesc(IMG); colormap(gray); colorbar; 
+ pause; 
+ 
 
-clear; % 変数のオールクリア
-
-ORG=imread('Lenna.png'); % 原画像の入力
-ORG= rgb2gray(ORG); % カラー画像を白黒濃淡画像へ変換
-
-imagesc(ORG); colormap(gray); colorbar; % 画像の表示
-pause;
-
-IMG = ORG > 64; % 輝度値が64以上の画素を1，その他を0に変換
-imagesc(IMG); colormap(gray); colorbar;
-pause;
-
-IMG = ORG > 96;
-imagesc(IMG); colormap(gray); colorbar;
-pause;
-
-IMG = ORG > 128;
-imagesc(IMG); colormap(gray); colorbar;
-pause;
-
-IMG = ORG > 192;
-imagesc(IMG); colormap(gray); colorbar;
-
+ IMG = ORG > 192; 
+ imagesc(IMG); colormap(gray); colorbar; 
