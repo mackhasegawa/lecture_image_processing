415e4fac16937e43ed1628fa37d648a08af40eee
Kouno <d86ysv3k@gmail.com>
2016-07-15 20:25:18 +0900

    Add files via upload

--- kadai4.m
+++ kadai4.m
@@ -1,18 +1,18 @@
-% 課題４　画像のヒストグラム
-% 画素の濃度ヒストグラムを生成せよ．
-% 下記はサンプルプログラムである．
-% 課題作成にあたっては「Lenna」以外の画像を用いよ．
-
-clear; % 変数のオールクリア
-
-ORG=imread('Lenna.png'); % 原画像の入力
-ORG=rgb2gray(ORG); % カラー画像を白黒濃淡画像へ変換
-imagesc(ORG); colormap(gray); colorbar;
-pause;
-
-imhist(ORG); % ヒストグラムの表示
-
-
-
-
-
+ % 課題４　画像のヒストグラム 
+ % 画素の濃度ヒストグラムを生成せよ． 
+ % 下記はサンプルプログラムである． 
+ % 課題作成にあたっては「Lenna」以外の画像を用いよ． 
+ 
+ 
+ clear; % 変数のオールクリア 
+ 
+ 
+ ORG=imread('http://ks.c.yimg.jp/res/chie-que-10141/10/141/933/305/i320'); % 原画像の入力 
+ ORG=rgb2gray(ORG); % カラー画像を白黒濃淡画像へ変換 
+ imagesc(ORG); colormap(gray); colorbar; 
+ pause; 
+ 
+ 
+ imhist(ORG); % ヒストグラムの表示 
+ 
+ 
