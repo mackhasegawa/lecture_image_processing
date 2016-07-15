517e944f3c0decbd404f31e6499f6879ebdbcaf0
Kouno <d86ysv3k@gmail.com>
2016-07-15 21:02:00 +0900

    Add files via upload

--- kadai2.m
+++ kadai2.m
@@ -16,7 +16,7 @@
  % ‚QŠK’²‰æ‘œ‚Ì¶¬ 
  IMG = ORG>128; 
  imagesc(IMG); colormap(gray); colorbar;  axis image; 
- pause; 
+ pause; % ˆêŽž’âŽ~  
  
  
  % ‚SŠK’²‰æ‘œ‚Ì¶¬ 
@@ -25,6 +25,18 @@
  IMG2 = ORG>192; 
  IMG = IMG0 + IMG1 + IMG2; 
  imagesc(IMG); colormap(gray); colorbar;  axis image; 
+  pause; % ˆêŽž’âŽ~ 
  
  
  % ‚WŠK’²‚É‚Â‚¢‚Ä‚ÍCŠeŽ©ŒŸ“¢‚µ‚Ä‚­‚¾‚³‚¢D 
+
+ IMG0 = ORG>32;
+ IMG1 = ORG>64;
+ IMG2 = ORG>96;
+ IMG3 = ORG>128;
+ IMG4 = ORG>160;
+ IMG5 = ORG>192;
+ IMG6 = ORG>224;
+ IMG = IMG0 + IMG1 + IMG2 + IMG3 + IMG4 + IMG5 + IMG6;
+ imagesc(IMG); colormap(gray); colorbar; axis image;
+ pause; % ˆêŽž’âŽ~ 
