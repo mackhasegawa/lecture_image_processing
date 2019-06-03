0b1013e7c80489823167ea131986127e77c6e6d7
Mitsuo Okada <mitsuo-okada@united.jp>
2019-06-03 21:39:13 +0900

    コメントを修正

--- kadai1.m
+++ kadai1.m
@@ -5,17 +5,17 @@ OriginalImageDir = 'images/origin/';
 OutputImageDir = 'images/kadai1/';
 OriginalImageFileName = 'ai-kato';
 OriginalImageFileExt = '.jpg';
-BoxRate1 = [1,2,4,8,16,32]
+ChangeScale = 0.5;
 
 % 原画像の入力
 OriginalImagePath = sprintf('%s%s%s', OriginalImageDir, OriginalImageFileName, OriginalImageFileExt);
 OriginalImage = imread(OriginalImagePath);
 IMG = OriginalImage;
 
-% Boxrateを元に画像をピクセル化して出力
+% Boxrateを元に画像をボックス化して出力
 for BoxRate = [1 2 4 8 16 32]
     OutputImagePath = sprintf('%s%s-box%02d%s', OutputImageDir, OriginalImageFileName, BoxRate, OriginalImageFileExt);
-    IMG = imresize(IMG, 0.5); % 画像の縮小
+    IMG = imresize(IMG, ChangeScale); % 画像の縮小
     IMG2 = imresize(IMG, BoxRate,'box'); % 画像の拡大
     disp(OutputImagePath);
     imwrite(IMG2, OutputImagePath);
